d5291bd25261008ade1a036f69e88ed011c72ee2
Geoguitar <geovanedsilveira@gmail.com>
2016-03-23 18:23:33 -0300

    implementado código

--- src/br/com/aulaThreads/printThreads/ImprimirThread_1.java
+++ src/br/com/aulaThreads/printThreads/ImprimirThread_1.java
@@ -1,5 +1,21 @@
 package br.com.aulaThreads.printThreads;
 
-public class ImprimirThread_1 {
-
+public class ImprimirThread_1 implements Runnable {
+	
+	String str;
+	
+	public ImprimirThread_1(String str){
+		
+		this.str = str;
+	}
+	
+	public void run() {
+		
+		for(int i = 0; i > 100; i++){
+			System.out.println(str);
+		}
+		
+	}
 }
+
+
